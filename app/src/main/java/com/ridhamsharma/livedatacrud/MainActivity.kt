d10ab893006b22fa3a68d881a7084c6ebd8ea885
Ridham32 <sharmaridham473@gmail.com>
2023-08-19 23:25:58 +0530

    ...

--- app/src/main/java/com/ridhamsharma/livedatacrud/MainActivity.kt
+++ app/src/main/java/com/ridhamsharma/livedatacrud/MainActivity.kt
@@ -2,9 +2,12 @@ package com.ridhamsharma.livedatacrud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewbinding.ViewBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
